6ef30f9689bd653960652afaad3770ea16ddbc2b
ardi515 <ardifirmansyah789@gmail.com>
2022-03-29 16:52:18 +0700

    menambahkan contoh 1 ,2 ,3

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -6,6 +6,6 @@ class Contoh1 extends CI_Controller
         echo "<h1>Perkenalkan</h1>";
         echo"Nama saya:Ardi Firmansyah
              saya tingaal di jl.kebon 200 <Rt010>
-             <Rw002></Rw002> oalahraga yg saya sukai adalah Sepak Bola";
+             <Rw002></Rw002> oalahraga yg saya suka adalah Sepak Bola";
     }
 }
