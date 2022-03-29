97d0f8a26053f5fb627d2f86466a5671fc1a3260
ardi515 <ardifirmansyah789@gmail.com>
2022-03-29 14:12:07 +0700

    menambahkan file.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya:Ardi Firmansyah
+             saya tingaal di jl.kebon 200 <Rt010>
+             <Rw002></Rw002> oalahraga yg saya sukai adalah Sepak Bola";
+    }
+}
